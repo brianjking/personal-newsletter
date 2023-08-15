5ffac8299792da449bec171d02a5443aaa95a4fb
Brian J King <brianjking@users.noreply.github.com>
2023-08-15 09:25:24 -0700

    Reverting to here to get rid of tracking, lets add branching to this mix, maybe.
    https://github.com/brianjking/personal-newsletter/blob/3a0c10034619dfe31810c28af02b6cfc740870cf/app.py

--- app.py
+++ app.py
@@ -10,6 +10,21 @@ from langchain.prompts import PromptTemplate
 from datetime import datetime
 from airtable import Airtable
 
+# Function to clear Airtable records
+def clear_airtable_records(api_key, base_key, table_name):
+    try:
+        airtable = Airtable(base_key, table_name, api_key)
+        records = airtable.get_all()
+        if records:
+            record_ids = [record['id'] for record in records]
+            for record_id in record_ids:
+                airtable.delete(record_id)
+            st.sidebar.success('URLs cleared successfully!')
+        else:
+            st.sidebar.warning('No URLs to clear.')
+    except Exception as e:
+        st.sidebar.error(f"An error occurred while clearing URLs: {str(e)}")
+
 # Secrets
 my_secret = os.environ['OPENAI_API_KEY']
 postmark_secret = os.environ['postmark_key']
@@ -20,8 +35,10 @@ AIRTABLE_API_KEY = os.environ['airtable_key']
 BASE_ID = st.secrets['BASE_ID']
 TABLE_NAME = os.environ['TABLE_NAME']
 
+# Initialize Airtable
 airtable = Airtable(BASE_ID, TABLE_NAME, api_key=AIRTABLE_API_KEY)
 
+# Streamlit UI
 st.title('Personal Newsletter Summarization')
 st.sidebar.title('Admin & Actions')
 
@@ -30,84 +47,81 @@ password = st.sidebar.text_input("Enter password:", type="password")
 correct_password = streamlit_key
 
 if password == correct_password:
+    # URL input
     url_input = st.text_input('Enter URL to add to add to the Airtable for processing for todays email summary:').strip()
     if url_input and st.button('Add URL'):
-        airtable.insert({'URL': url_input})
-        st.success('URL added successfully!')
+        try:
+            airtable.insert({'URL': url_input})
+            st.success('URL added successfully!')
+        except Exception as e:
+            st.error(f"An error occurred while adding URL: {str(e)}")
 
+    # View URLs
     if st.sidebar.button('View URLs'):
-        records = airtable.get_all()
-        urls = [record['fields']['URL'] for record in records if 'URL' in record['fields']]
-        st.write(urls)
+        try:
+            records = airtable.get_all()
+            urls = [record['fields']['URL'] for record in records if 'URL' in record['fields']]
+            st.write(urls)
+        except Exception as e:
+            st.error(f"An error occurred while fetching URLs: {str(e)}")
 
+    # Execute Summarization
     if st.sidebar.button('Execute Summarization'):
-        st.sidebar.success('Summarization process started...')
-        urls = [record['fields']['URL'] for record in airtable.get_all() if 'URL' in record['fields']]
-        all_summaries = ""
+        try:
+            st.sidebar.success('Summarization process started...')
+            urls = [record['fields']['URL'] for record in airtable.get_all() if 'URL' in record['fields']]
+            all_summaries = ""
 
-        # Custom Prompt Template
-        prompt_template = """Write a high-level executive summary of the following text, and then list the vital key points in bullet form. The summary should serve as a TL/DR for the content and contain the most important information. If there are topics that focus on marketing, local marketing, brand compliance, brand voice, marketing or similar topics included in the documents be sure to include these in the summary as they will be interesting to the BrandMuscle employee who reads the summary. If the document text does not focus on these topics you can include a section that talks about how to apply the information to local marketing.
+            # Custom Prompt Template
+            prompt_template = """Write a high-level executive summary of the following text, and then list the vital key points in bullet form. The summary should serve as a TL/DR for the content and contain the most important information. If there are topics that focus on marketing, local marketing, brand compliance, brand voice, marketing or similar topics included in the documents be sure to include these in the summary as they will be interesting to the BrandMuscle employee who reads the summary. If the document text does not focus on these topics you can include a section that talks about how to apply the information to local marketing.
 
-        {text}
+            {text}
 
-        SUMMARY:"""
-        PROMPT = PromptTemplate.from_template(prompt_template)
+            SUMMARY:"""
+            PROMPT = PromptTemplate.from_template(prompt_template)
 
-        # Initializing LLM
-        llm = ChatOpenAI(openai_api_key=my_secret,
-                        temperature=0,
-                        model_name="gpt-3.5-turbo-16k")
+            # Summarization code
+            for index, url in enumerate(urls, 1):
+                print(f"Loading content from URL: {url.strip()}...")
+                loader = WebBaseLoader(url.strip())
+                docs = loader.load()
 
-        llm_chain = LLMChain(llm=llm, prompt=PROMPT)
+                print("Initializing LLM...")
+                llm = ChatOpenAI(openai_api_key=my_secret,
+                                temperature=0,
+                                model_name="gpt-3.5-turbo-16k")
 
-        # Define the chain
-        chain = StuffDocumentsChain(llm_chain=llm_chain,
-                                    document_variable_name="text")
+                llm_chain = LLMChain(llm=llm, prompt=PROMPT)
 
-        for index, url in enumerate(urls, 1):
-            print(f"Loading content from URL: {url.strip()}...")
-            loader = WebBaseLoader(url.strip())
-            docs = loader.load()
+                print("Loading and running summarization chain...")
+                chain = StuffDocumentsChain(llm_chain=llm_chain,
+                                            document_variable_name="text")
+                summary = chain.run(docs)
 
-            print("Loading and running summarization chain...")
-            response = chain.run(docs)
-            summary = response['choices'][0]['text'].strip()
+                print("Storing summary in a file...")
+                all_summaries += f"{index}. {url.strip()}\n{summary}\n\n"
 
-            print("Storing summary in a file...")
-            all_summaries += f"{index}. {url.strip()}\n{summary}\n\n"
+            # Sending summaries via email
+            sender_email = sender_key
+            receiver_email = receiver_key
+            current_date = datetime.today().strftime('%Y-%m-%d')
+            subject = f"Daily Summaries - {current_date}"
+            message = MIMEText(all_summaries)
+            message["Subject"] = subject
+            message["From"] = sender_email
+            message["To"] = receiver_email
 
-            # Calculate tokens and cost
-            tokens_used, cost = calculate_tokens_and_cost(response)
+            with smtplib.SMTP("smtp.postmarkapp.com", 587) as server:
+                server.starttls()
+                server.login(postmark_secret, postmark_secret)
+                server.sendmail(sender_email, receiver_email, message.as_string())
 
-            # Log the tokens and cost
-            st.write(f"Tokens used: {tokens_used}")
-            st.write(f"Cost: ${cost:.4f}")
+            st.sidebar.success('Summarization process completed!')
+        except Exception as e:
+            st.sidebar.error(f"An error occurred during summarization: {str(e)}")
 
-        # Sending summaries via email
-        sender_email = sender_key
-        receiver_email = receiver_key
-        current_date = datetime.today().strftime('%Y-%m-%d')
-        subject = f"Daily Summaries - {current_date}"
-        message = MIMEText(all_summaries)
-        message["Subject"] = subject
-        message["From"] = sender_email
-        message["To"] = receiver_email
-
-        with smtplib.SMTP("smtp.postmarkapp.com", 587) as server:
-            server.starttls()
-            server.login(postmark_secret, postmark_secret)
-            server.sendmail(sender_email, receiver_email, message.as_string())
-
-        st.sidebar.success('Summarization process completed!')
-
+    # Clear URLs
     if st.sidebar.button('Clear URLs'):
-        records = airtable.get_all()
-        if records:
-            record_ids = [record['id'] for record in records]
-            for record_id in record_ids:
-                airtable.delete(record_id)
-            st.sidebar.success('URLs cleared successfully!')
-        else:
-            st.sidebar.warning('No URLs to clear.')
+        clear_airtable_records(AIRTABLE_API_KEY, BASE_ID, TABLE_NAME)
 else:
     st.sidebar.warning('Incorrect password. Please enter the correct password to proceed.')
