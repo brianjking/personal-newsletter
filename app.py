864c32a73479f3b6c2636794612cba9c6e643a00
Brian J King <brianjking@users.noreply.github.com>
2023-08-15 08:46:54 -0700

    trying tracking approach #2
    https://python.langchain.com/docs/modules/model_io/models/llms/token_usage_tracking

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import WebBaseLoader
 from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 from datetime import datetime
 from airtable import Airtable
 
@@ -24,13 +26,6 @@ airtable = Airtable(BASE_ID, TABLE_NAME, api_key=AIRTABLE_API_KEY)
 
 st.title('Personal Newsletter Summarization')
 st.sidebar.title('Admin & Actions')
-
-# Function to calculate tokens and cost
-def calculate_tokens_and_cost(response):
-    tokens_used = response['usage']['total_tokens']
-    cost_per_token = 0.06 / 1000  # Assuming $0.06 per 1000 tokens, adjust this value based on your pricing
-    cost = tokens_used * cost_per_token
-    return tokens_used, cost
 
 # Password protection
 password = st.sidebar.text_input("Enter password:", type="password")
@@ -51,8 +46,6 @@ if password == correct_password:
         st.sidebar.success('Summarization process started...')
         urls = [record['fields']['URL'] for record in airtable.get_all() if 'URL' in record['fields']]
         all_summaries = ""
-        total_tokens_used = 0
-        total_cost = 0.0
 
         # Custom Prompt Template
         prompt_template = """Write a high-level executive summary of the following text, and then list the vital key points in bullet form. The summary should serve as a TL/DR for the content and contain the most important information. If there are topics that focus on marketing, local marketing, brand compliance, brand voice, marketing or similar topics included in the documents be sure to include these in the summary as they will be interesting to the BrandMuscle employee who reads the summary. If the document text does not focus on these topics you can include a section that talks about how to apply the information to local marketing.
@@ -62,30 +55,28 @@ if password == correct_password:
         SUMMARY:"""
         PROMPT = PromptTemplate.from_template(prompt_template)
 
-        for index, url in enumerate(urls, 1):
-            print(f"Loading content from URL: {url.strip()}...")
-            loader = WebBaseLoader(url.strip())
-            docs = loader.load()
+        # Initialize the LLM
+        llm = OpenAI(model_name="gpt-3.5-turbo-16k", n=2, best_of=2) # Adjust the model_name and other parameters as needed
 
-            print("Initializing LLM...")
-            llm = ChatOpenAI(openai_api_key=my_secret,
-                            temperature=0,
-                            model_name="gpt-3.5-turbo-16k")
+        # Wrap the calls inside the context manager
+        with get_openai_callback() as cb:
+            for index, url in enumerate(urls, 1):
+                print(f"Loading content from URL: {url.strip()}...")
+                loader = WebBaseLoader(url.strip())
+                docs = loader.load()
 
-            llm_chain = LLMChain(llm=llm, prompt=PROMPT)
+                print("Initializing LLM...")
+                llm_chain = LLMChain(llm=llm, prompt=PROMPT)
 
-            print("Loading and running summarization chain...")
-            chain = StuffDocumentsChain(llm_chain=llm_chain,
-                                        document_variable_name="text")
-            summary, response = chain.run(docs)  # Assuming chain.run returns the response as well
+                print("Loading and running summarization chain...")
+                chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
+                summary = chain.run(docs)
 
-            # Calculate tokens and cost
-            tokens_used, cost = calculate_tokens_and_cost(response)
-            total_tokens_used += tokens_used
-            total_cost += cost
+                print("Storing summary in a file...")
+                all_summaries += f"{index}. {url.strip()}\n{summary}\n\n"
 
-            print("Storing summary in a file...")
-            all_summaries += f"{index}. {url.strip()}\n{summary}\n\n"
+            print(f"Tokens Used: {cb.total_tokens}")
+            print(f"Total Cost (USD): ${cb.total_cost}")
 
         # Sending summaries via email
         sender_email = sender_key
@@ -104,18 +95,14 @@ if password == correct_password:
 
         st.sidebar.success('Summarization process completed!')
 
-        # Log the tokens and cost
-        st.write(f"Total Tokens used: {total_tokens_used}")
-        st.write(f"Total Cost: ${total_cost:.4f}")
-
-        if st.sidebar.button('Clear URLs'):
-            records = airtable.get_all()
-            if records:
-                record_ids = [record['id'] for record in records]
-                for record_id in record_ids:
-                    airtable.delete(record_id)
-                st.sidebar.success('URLs cleared successfully!')
-            else:
-                st.sidebar.warning('No URLs to clear.')
+    if st.sidebar.button('Clear URLs'):
+        records = airtable.get_all()
+        if records:
+            record_ids = [record['id'] for record in records]
+            for record_id in record_ids:
+                airtable.delete(record_id)
+            st.sidebar.success('URLs cleared successfully!')
+        else:
+            st.sidebar.warning('No URLs to clear.')
 else:
     st.sidebar.warning('Incorrect password. Please enter the correct password to proceed.')
