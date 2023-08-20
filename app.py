9f10d11efcda53dc9f5a47490462ef6128beb84b
Brian J King <brianjosephking@gmail.com>
2023-08-19 17:43:08 -0700

    FUCK

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from datetime import datetime
 from airtable import Airtable
 from largeContext import sliding_window, process_windows, combine_summaries
 
-# Function to clear Airtable records
 def clear_airtable_records(api_key, base_key, table_name):
     try:
         airtable = Airtable(base_key, table_name, api_key)
@@ -26,7 +25,6 @@ def clear_airtable_records(api_key, base_key, table_name):
     except Exception as e:
         st.sidebar.error(f"An error occurred while clearing URLs: {str(e)}")
 
-# Secrets
 my_secret = st.secrets['OPENAI_API_KEY']
 postmark_secret = st.secrets['postmark_key']
 sender_key = st.secrets['sender_key']
@@ -36,19 +34,15 @@ AIRTABLE_API_KEY = st.secrets['airtable_key']
 BASE_ID = st.secrets['BASE_ID']
 TABLE_NAME = st.secrets['TABLE_NAME']
 
-# Initialize Airtable
 airtable = Airtable(BASE_ID, TABLE_NAME, api_key=AIRTABLE_API_KEY)
 
-# Streamlit UI
 st.title('Personal Newsletter Summarization')
 st.sidebar.title('Admin & Actions')
 
-# Password protection
 password = st.sidebar.text_input("Enter password:", type="password")
 correct_password = streamlit_key
 
 if password == correct_password:
-    # URL input
     url_input = st.text_input('Enter URL to add to add to the Airtable for processing for todays email summary:').strip()
     if url_input and st.button('Add URL'):
         try:
@@ -57,7 +51,6 @@ if password == correct_password:
         except Exception as e:
             st.error(f"An error occurred while adding URL: {str(e)}")
 
-    # View URLs
     if st.sidebar.button('View URLs'):
         try:
             records = airtable.get_all()
@@ -66,14 +59,11 @@ if password == correct_password:
         except Exception as e:
             st.error(f"An error occurred while fetching URLs: {str(e)}")
 
-    # Execute Summarization
     if st.sidebar.button('Execute Summarization'):
         try:
             st.sidebar.success('Summarization process started...')
             urls = [record['fields']['URL'] for record in airtable.get_all() if 'URL' in record['fields']]
             all_summaries = ""
-
-            # Custom Prompt Template
             prompt_template = """Write a high-level executive summary of the following text, and then list the vital key points in bullet form. The summary should serve as a TL/DR for the content and contain the most important information. If there are topics that focus on marketing, local marketing, brand compliance, brand voice, marketing or similar topics included in the documents be sure to include these in the summary as they will be interesting to the BrandMuscle employee who reads the summary. If the document text does not focus on these topics you can include a section that talks about how to apply the information to local marketing.
 
             {text}
@@ -86,6 +76,10 @@ if password == correct_password:
                 print(f"Loading content from URL: {url.strip()}...")
                 loader = WebBaseLoader(url.strip())
                 docs = loader.load()
+
+                # Debugging code to print the type and content of each doc.page_content
+                for doc in docs:
+                    print(type(doc.page_content), doc.page_content)
 
                 print("Initializing LLM...")
                 llm = ChatOpenAI(openai_api_key=my_secret,
@@ -111,26 +105,24 @@ if password == correct_password:
                 print("Storing summary in a file...")
                 all_summaries += f"{index}. {url.strip()}\n{summary}\n\n"
 
-                # Sending summaries via email
-                sender_email = sender_key
-                receiver_email = receiver_key
-                current_date = datetime.today().strftime('%Y-%m-%d')
-                subject = f"Daily Summaries - {current_date}"
-                message = MIMEText(all_summaries)
-                message["Subject"] = subject
-                message["From"] = sender_email
-                message["To"] = receiver_email
+            sender_email = sender_key
+            receiver_email = receiver_key
+            current_date = datetime.today().strftime('%Y-%m-%d')
+            subject = f"Daily Summaries - {current_date}"
+            message = MIMEText(all_summaries)
+            message["Subject"] = subject
+            message["From"] = sender_email
+            message["To"] = receiver_email
 
-                with smtplib.SMTP("smtp.postmarkapp.com", 587) as server:
-                    server.starttls()
-                    server.login(postmark_secret, postmark_secret)
-                    server.sendmail(sender_email, receiver_email, message.as_string())
+            with smtplib.SMTP("smtp.postmarkapp.com", 587) as server:
+                server.starttls()
+                server.login(postmark_secret, postmark_secret)
+                server.sendmail(sender_email, receiver_email, message.as_string())
 
             st.sidebar.success('Summarization process completed!')
         except Exception as e:
             st.sidebar.error(f"An error occurred during summarization: {str(e)}")
 
-    # Clear URLs
     if st.sidebar.button('Clear URLs'):
         clear_airtable_records(AIRTABLE_API_KEY, BASE_ID, TABLE_NAME)
 else:
